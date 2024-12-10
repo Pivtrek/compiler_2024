34fff4390dfb505fc0b276d83aa4e921801a3c86
Pivtrek <piotrek.bijacz.7@gmail.com>
2024-12-10 13:54:33 +0100

    Created Symbol structure

--- src/main/java/org/example/semantic/Symbol.java
+++ src/main/java/org/example/semantic/Symbol.java
@@ -1,4 +1,11 @@
 package org.example.semantic;
 
 public class Symbol {
+    private String name;
+    private String type;
+
+    public Symbol(String name, String type){
+        this.name = name;
+        this.type = type;
+    }
 }
