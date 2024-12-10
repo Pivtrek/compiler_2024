998a2c86fce503fcf4ff754e216500667fb3e2bd
Pivtrek <piotrek.bijacz.7@gmail.com>
2024-12-10 14:16:10 +0100

    Created constructor for symbol Table

--- src/main/java/org/example/semantic/SymbolTable.java
+++ src/main/java/org/example/semantic/SymbolTable.java
@@ -1,4 +1,11 @@
 package org.example.semantic;
+import java.util.HashSet;
 
 public class SymbolTable {
+
+    private HashSet<Symbol> symbolTable;
+    public SymbolTable() {
+        symbolTable = new HashSet<Symbol>();
+    }
+
 }
