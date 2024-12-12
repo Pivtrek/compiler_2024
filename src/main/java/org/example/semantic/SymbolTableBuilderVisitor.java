b1018ac3e02b422aea7be5827aba6a5e9e2b5d9e
Pivtrek <piotrek.bijacz.7@gmail.com>
2024-12-12 21:32:53 +0100

    processDeclaration function is working properly

--- src/main/java/org/example/semantic/SymbolTableBuilderVisitor.java
+++ src/main/java/org/example/semantic/SymbolTableBuilderVisitor.java
@@ -50,8 +50,13 @@ public class SymbolTableBuilderVisitor extends GrammarBaseVisitor<Void> {
 
         visit(ctx.declarations());
 
+        GrammarParser.DeclarationsContext declCtx = ctx.declarations();
 
+        if (declCtx != null){
+            processDeclarations(declCtx, procedure_with_variables);
+        }
 
+        symbolTable.addSymbol(procedure_with_variables);
 
         return null;
     }
@@ -61,15 +66,11 @@ public class SymbolTableBuilderVisitor extends GrammarBaseVisitor<Void> {
         if (ctx instanceof GrammarParser.ARGSMULTIDECLContext argument_context) {
             procedure.addParameter(new Symbol(argument_context.PIDENTIFIER().getText(), Symbol.SymbolType.INT));
             processArguments(argument_context.args_decl(), procedure);
-
         } else if (ctx instanceof GrammarParser.ARGSDECLContext argument_context) {
             procedure.addParameter(new Symbol(argument_context.PIDENTIFIER().getText(), Symbol.SymbolType.INT));
-
         } else if (ctx instanceof GrammarParser.ARGSMUTLIARRDECLContext array_context) {
-
             procedure.addParameter(new Symbol(array_context.PIDENTIFIER().getText(), Symbol.SymbolType.ARRAY));
             processArguments(array_context.args_decl(), procedure);
-
         } else if (ctx instanceof GrammarParser.ARGSARRDECLContext array_context) {
             procedure.addParameter(new Symbol(array_context.PIDENTIFIER().getText(), Symbol.SymbolType.ARRAY));
         }
@@ -77,23 +78,23 @@ public class SymbolTableBuilderVisitor extends GrammarBaseVisitor<Void> {
 
     private void processDeclarations(GrammarParser.DeclarationsContext ctx, Symbol procedure){
         if (ctx instanceof GrammarParser.MULTISINGLEDECLARATIONContext argument_context){
-            procedure.addParameter(new Symbol(argument_context.PIDENTIFIER().getText(), Symbol.SymbolType.INT));
+            procedure.addLocalVariable(new Symbol(argument_context.PIDENTIFIER().getText(), Symbol.SymbolType.INT));
             processDeclarations(argument_context.declarations(), procedure);
         } else if (ctx instanceof GrammarParser.SINGLEDECLARATIONContext argument_context) {
-            procedure.addParameter(new Symbol(argument_context.PIDENTIFIER().getText(), Symbol.SymbolType.INT));
+            procedure.addLocalVariable(new Symbol(argument_context.PIDENTIFIER().getText(), Symbol.SymbolType.INT));
         } else if (ctx instanceof GrammarParser.MULTIARRAYDECLARATIONContext array_context) {
             Symbol array  = new Symbol(array_context.PIDENTIFIER().getText(), Symbol.SymbolType.ARRAY);
             int lower_bound = Integer.parseInt(array_context.NUM(0).toString());
             int upper_bound = Integer.parseInt(array_context.NUM(1).toString());
             array.setArrayBounds(lower_bound, upper_bound);
-            procedure.addParameter(array);
+            procedure.addLocalVariable(array);
             processDeclarations(array_context.declarations(), procedure);
         } else if (ctx instanceof GrammarParser.ARRAYDECLARATIONContext array_context) {
             Symbol array  = new Symbol(array_context.PIDENTIFIER().getText(), Symbol.SymbolType.ARRAY);
             int lower_bound = Integer.parseInt(array_context.NUM(0).toString());
             int upper_bound = Integer.parseInt(array_context.NUM(1).toString());
             array.setArrayBounds(lower_bound, upper_bound);
-            procedure.addParameter(array);
+            procedure.addLocalVariable(array);
         }
     }
 
