e3537504165ad3988afa8c5de1a774417501dfc7
Pivtrek <piotrek.bijacz.7@gmail.com>
2024-12-13 21:06:30 +0100

    Created methods for adding, and getting error messsages, and getting boolean if list has errors to decide for further procide

--- src/main/java/org/example/semantic/ErrorColector.java
+++ src/main/java/org/example/semantic/ErrorColector.java
@@ -1,4 +1,35 @@
 package org.example.semantic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ErrorColector {
+    public static class SemanticError{
+        private final String message;
+        private final int line;
+
+        public SemanticError(String message, int line) {
+            this.message = message;
+            this.line = line;
+        }
+
+        @Override
+        public String toString() {
+            return "BŁĄD w linii: " + line + " : " + message;
+        }
+    }
+
+    private final List<SemanticError> errors = new ArrayList<>();
+
+    public void addError(String message, int line){
+        errors.add(new SemanticError(message, line));
+    }
+
+    public boolean hasError(){
+        return !errors.isEmpty();
+    }
+
+    public List<SemanticError> getErrors(){
+        return errors;
+    }
 }
