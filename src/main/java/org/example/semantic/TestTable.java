f0c8b2323abb3d041bf2d94d301794dc4d7bd6ae
Pivtrek <piotrek.bijacz.7@gmail.com>
2024-12-16 19:53:18 +0100

    Modified test table

--- src/main/java/org/example/semantic/TestTable.java
+++ src/main/java/org/example/semantic/TestTable.java
@@ -16,7 +16,7 @@ import java.nio.file.Paths;
 public class TestTable {
     public static void main(String[] args) {
 
-        String filePath = "examples/example8.imp";
+        String filePath = "examples/errors/error9.imp";
 
         try {
             // 1. Odczyt kodu z pliku
