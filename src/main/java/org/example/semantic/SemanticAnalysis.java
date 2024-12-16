6e142100b742cb86ad4b083cc9584b2d3995e398
Pivtrek <piotrek.bijacz.7@gmail.com>
2024-12-16 20:47:10 +0100

    created constructor

--- src/main/java/org/example/semantic/SemanticAnalysis.java
+++ src/main/java/org/example/semantic/SemanticAnalysis.java
@@ -1,5 +1,11 @@
 package org.example.semantic;
 
 public class SemanticAnalysis {
+    private final SymbolTable symbolTable;
+    private final ErrorColector errorColector;
 
+    public SemanticAnalysis(SymbolTable symbolTable, ErrorColector errorColector) {
+        this.symbolTable = symbolTable;
+        this.errorColector = errorColector;
+    }
 }
