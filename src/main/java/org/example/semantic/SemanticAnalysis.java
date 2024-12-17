3db147bc3355af4d581d917fdb4aac84c37fd105
Pivtrek <piotrek.bijacz.7@gmail.com>
2024-12-17 22:42:49 +0100

    For now no Semantic Analysis

--- src/main/java/org/example/semantic/SemanticAnalysis.java
+++ src/main/java/org/example/semantic/SemanticAnalysis.java
@@ -12,23 +12,6 @@ public class SemanticAnalysis extends GrammarBaseVisitor<Void> {
     }
 
     public void analyze(){
-        checkForWrongLocalVariables();
-    }
 
-    //Checking if local variables in procedure are the same as arguments
-    private void checkForWrongLocalVariables(){
-        for (Symbol symbol: symbolTable.getALlSymbols()) {
-            System.out.println(symbol);
-            if (symbol.getType() == Symbol.SymbolType.PROCEDURE_WITH_LOCAL_VARIABLES){
-                for (Symbol parameter : symbol.getParameters()){
-                    for (Symbol local_variable : symbol.getLocalVariables()){
-                        if (parameter.getName().equals(local_variable.getName())){
-                            errorColector.reportError("Powtórne użycie identyfikatora " + parameter.getName(), 1); //TODO: BRAK LINI WIĘC TRZEBA TO ZAIMPLEMENTOWAĆ PRZY TWORZENIU PROCEDURY
-                        }
-                    }
-                }
-            }
-        }
     }
-
 }
