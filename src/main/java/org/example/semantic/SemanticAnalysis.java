79e47699fa788ab6a117531fc3ebb9a9efdcd96a
Pivtrek <piotrek.bijacz.7@gmail.com>
2024-12-22 16:43:09 +0100

    Creating function to find bad usages in assign

--- src/main/java/org/example/semantic/SemanticAnalysis.java
+++ src/main/java/org/example/semantic/SemanticAnalysis.java
@@ -1,6 +1,8 @@
 package org.example.semantic;
 
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.example.parser.GrammarBaseVisitor;
+import org.example.parser.GrammarParser;
 
 public class SemanticAnalysis extends GrammarBaseVisitor<Void> {
     private final SymbolTable symbolTable;
@@ -13,5 +15,35 @@ public class SemanticAnalysis extends GrammarBaseVisitor<Void> {
 
     public void analyze(){
 
+    }
+
+
+    @Override
+    public Void visitASSIGN(GrammarParser.ASSIGNContext ctx) {
+
+        System.out.println(ctx.getText() + "PROCEDURE - " + findEnclosingScope(ctx));
+
+        return null;
+    }
+
+    private String findEnclosingScope(ParserRuleContext context){
+        ParserRuleContext current = context;
+
+        while (current != null){
+            if (current instanceof GrammarParser.PROCEDUREWITHDECLARATIONSContext){
+                return ((GrammarParser.PROCEDUREWITHDECLARATIONSContext) current).proc_head().PIDENTIFIER().getText();
+            }
+            if (current instanceof GrammarParser.PROCEDUREWITHOUTDECLARATIONSContext){
+                return ((GrammarParser.PROCEDUREWITHOUTDECLARATIONSContext) current).proc_head().PIDENTIFIER().getText();
+            }
+            if (current instanceof GrammarParser.MAINDECLARATIONSContext){
+                return "PROGRAM_IS_DECLARATIONS";
+            }
+            if (current instanceof GrammarParser.MAINWITHOUTDECLARATIONSContext){
+                return "PROGRAM_IS";
+            }
+            current = current.getParent();
+        }
+        return "UKNOWN SCOPE";
     }
 }
