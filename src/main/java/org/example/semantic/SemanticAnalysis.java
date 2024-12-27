85993709fe82be5a941f85f01c64f2e9d723a2ec
Pivtrek <piotrek.bijacz.7@gmail.com>
2024-12-27 11:46:49 +0100

    Assign function now follow initilize value for local variables

--- src/main/java/org/example/semantic/SemanticAnalysis.java
+++ src/main/java/org/example/semantic/SemanticAnalysis.java
@@ -21,6 +21,19 @@ public class SemanticAnalysis extends GrammarBaseVisitor<Void> {
     }
 
     @Override
+    public Void visitASSIGN(GrammarParser.ASSIGNContext ctx) {
+        //TODO: MARK VARIABLE AS ASSIGNED AND THEN PROCCED TO INT USAGE AND CHECK IF ITS ASSIGN IF NOT RAISE EROOR
+        String procedure_name = findEnclosingScope(ctx);
+        if (ctx.identifier() instanceof GrammarParser.INTUSAGEContext && symbolTable.getSymbol(procedure_name).getLocalVariables() != null){
+            for (Symbol localVariable: symbolTable.getSymbol(procedure_name).getLocalVariables()){
+                if (localVariable.getName().equals(ctx.identifier().getText())){
+                    localVariable.setInitialized(true);
+                }
+            }
+        }
+        return null;
+    }
+    @Override
     public Void visitREAD(GrammarParser.READContext ctx) {
         String procedure_name = findEnclosingScope(ctx);
         if (symbolTable.containsSymbol(procedure_name)){
@@ -33,12 +46,6 @@ public class SemanticAnalysis extends GrammarBaseVisitor<Void> {
             }
         }
         return null;
-    }
-
-    @Override
-    public Void visitASSIGN(GrammarParser.ASSIGNContext ctx) {
-        //TODO: MARK VARIABLE AS ASSIGNED AND THEN PROCCED TO INT USAGE AND CHECK IF ITS ASSIGN IF NOT RAISE EROOR
-        return super.visitASSIGN(ctx);
     }
 
     @Override
--- src/main/java/org/example/semantic/TestTable.java
+++ src/main/java/org/example/semantic/TestTable.java
@@ -16,7 +16,7 @@ import java.nio.file.Paths;
 public class TestTable {
     public static void main(String[] args) throws IOException {
 
-        String filePath = "examples/errors/error2.imp";
+        String filePath = "examples/errors/error10.imp";
 
         try {
             // 1. Odczyt kodu z pliku
