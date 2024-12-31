eaf62e805f75c508c50124a7a379191ae51773d5
Pivtrek <piotrek.bijacz.7@gmail.com>
2024-12-31 16:25:41 +0100

    Created basic constructor memcell

--- src/main/java/org/example/memory/MemCell.java
+++ src/main/java/org/example/memory/MemCell.java
@@ -1,4 +1,18 @@
 package org.example.memory;
 
+import java.util.Map;
+
 public class MemCell {
+    private String name, scope;
+    private int registerNumber, value;
+    private boolean isArray, isRegister;
+    private Map<Integer, Integer> arrayIndexes;
+
+    public MemCell(String name, String scope, int registerNumber, boolean isArray, boolean isRegister) {
+        this.name = name;
+        this.scope = scope;
+        this.registerNumber = registerNumber;
+        this.isArray = isArray;
+        this.isRegister = isRegister;
+    }
 }
