cb81dfa2e5037ca2c26331d375a0ad982666e373
Pivtrek <piotrek.bijacz.7@gmail.com>
2025-01-03 13:25:10 +0100

    testtable

--- src/main/java/org/example/semantic/TestTable.java
+++ src/main/java/org/example/semantic/TestTable.java
@@ -17,7 +17,7 @@ import java.nio.file.Paths;
 public class TestTable {
     public static void main(String[] args) throws IOException {
 
-        String filePath = "examples/example1.imp";
+        String filePath = "examples/example4.imp";
 
         try {
             // 1. Odczyt kodu z pliku
