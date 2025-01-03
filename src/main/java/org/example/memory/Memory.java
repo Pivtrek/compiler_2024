badc39518bf567990c6c455cc33d566d19b26a58
Pivtrek <piotrek.bijacz.7@gmail.com>
2025-01-03 13:19:44 +0100

    Added for by symbol table

--- src/main/java/org/example/memory/Memory.java
+++ src/main/java/org/example/memory/Memory.java
@@ -1,6 +1,8 @@
 package org.example.memory;
+import org.example.semantic.Symbol;
 import org.example.semantic.SymbolTable;
 import java.util.Map;
+import java.util.SimpleTimeZone;
 
 public class Memory {
     private Map<String, MemCell> memory;
@@ -13,7 +15,12 @@ public class Memory {
 
     //Key format for name in memory 'name:scope'
     private void initializeFromSymbolTable(SymbolTable symbolTable){
+        for (Map.Entry<String, Symbol> entry: symbolTable.getALlSymbols().entrySet()){
+            String procedureName = entry.getKey();
+            Symbol procedure = entry.getValue();
 
+            System.out.println(procedureName + "---" + procedure);
+        }
     }
 
     private void addMemCell(){
