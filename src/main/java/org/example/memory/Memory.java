5d6dc5c9f034798c51c934bbbd363422a274ba49
Pivtrek <piotrek.bijacz.7@gmail.com>
2025-01-03 11:37:06 +0100

    Set constructor of Memory and Map memory, register and integer for nextFreeAdress

--- src/main/java/org/example/memory/Memory.java
+++ src/main/java/org/example/memory/Memory.java
@@ -1,4 +1,12 @@
 package org.example.memory;
 
+import java.util.Map;
+
 public class Memory {
+    private Map<Integer, MemCell> memory;
+    private Map<Integer, MemCell> register;
+    int nextFreeAdress;
+
+    public Memory() {
+    }
 }
