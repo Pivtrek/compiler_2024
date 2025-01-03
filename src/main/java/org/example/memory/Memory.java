fafc46a066afeedf190243c37bc426ceea4068af
Pivtrek <piotrek.bijacz.7@gmail.com>
2025-01-03 11:51:14 +0100

    added functions for adding memcell and initliazing mem from symboltable

--- src/main/java/org/example/memory/Memory.java
+++ src/main/java/org/example/memory/Memory.java
@@ -1,5 +1,5 @@
 package org.example.memory;
-
+import org.example.semantic.SymbolTable;
 import java.util.Map;
 
 public class Memory {
@@ -8,5 +8,13 @@ public class Memory {
     int nextFreeAdress = 8;
 
     public Memory() {
+    }
+
+    public void initializeFromSymbolTable(SymbolTable symbolTable){
+
+    }
+
+    private void addMemCell(){
+
     }
 }
