2648fee62ebad593fd7885130ebe340e81535b43
Pivtrek <piotrek.bijacz.7@gmail.com>
2025-02-03 11:11:45 +0100

    String array with test names

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class Test {
+    public static void main(String[] args) {
+        String[] names = {"example1.imp", "example2.imp", "example3.imp", "example4.imp", "example5.imp", "example6.imp", "example7.imp", "example8.imp", "example9.imp", "exampleA-n.imp", "program0.imp", "program1.imp", "program2.imp", "program3.imp"};
+
+    }
 }
